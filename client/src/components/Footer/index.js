ff3f3b6ea995b8d118cc2a76e2a7fa093fc198aa
unknown <raytakemura@gmail.com>
2021-08-13 17:08:40 -0700

    footer animation on load

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,10 +1,15 @@
 import './index.css'
 import github from '../../assets/icons/github.svg';
 import linkedin from '../../assets/icons/linkedin.svg';
+import { motion } from 'framer-motion';
 
 const Footer = () => {
     return (
-        <footer className=" d-flex justify-content-between">
+        <motion.footer className=" d-flex justify-content-between"
+            initial={{y: 250 }}
+            animate={{ y: 0 }}
+            transition={{ delay: 0.5, type: 'spring', stiffness: 30 }}
+        >
             <div>
                 <a href="https://www.linkedin.com/in/ray-takemura-57156b18b/">
                     <img style={{width:"28px"}} className="linkedin" src={linkedin} alt="linkedin"/>
@@ -19,7 +24,7 @@ const Footer = () => {
                 <span className="align-middle">&copy; Ray Takemura</span>
             </div>
             
-        </footer>
+        </motion.footer>
     )
 }
 
