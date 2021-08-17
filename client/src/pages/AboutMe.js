904d82947b8f931485d2ef14adfd226dbba15967
unknown <raytakemura@gmail.com>
2021-08-16 23:17:13 -0700

    update the about me description

--- client/src/pages/AboutMe.js
+++ client/src/pages/AboutMe.js
@@ -21,13 +21,7 @@ const AboutMe = () => {
                     </Col>
                     <Col lg={8}>
                         <p className="prof-doc">
-                            I'm a full-stack web developer with excellent problem-solving skills; 
-                            I have strong attention to detail with the ability to deliver appropriate 
-                            and efficient solutions. My critical thinking ability and hungriness 
-                            to solve problems have allowed my teams to finish our projects 
-                            quickly and elegantly. The finished projects have user-friendly 
-                            UI with many use-cases tested. Learning new skills and library is 
-                            exciting and I am ready to learn more every day.
+                        I'm a full-stack web developer with excellent problem-solving skills. I have a strong attention to detail with the ability to deliver appropriate and efficient solutions. My critical thinking ability and hunger to solve problems have allowed my teams to finish our projects quickly and elegantly. The finished projects have user-friendly UI with many use cases tested. As shown in my field of study, I enjoy learning and growing. I am eager to learn more every day.
                         </p>
                     </Col>
                 </Row>
